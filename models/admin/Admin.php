38c4bf07b2825ac1529f0b61dcc9d99b755463dc
xayru110 <sdxmuzb@gmail.com>
2020-05-10 16:54:30 +0500

    mustaqil ish

---
+++ models/admin/Admin.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * 
+ */
+
+/**
+ *
+ */
+
+class Admin
+{
+    private $id;
+    private $name;
+    private $pass;
+    private static $admins=[];
+
+    /**
+     * Admin constructor.
+     * @param $id
+     * @param $name
+     * @param $pass
+     */
+    public function __construct ($id, $name, $pass)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->pass = $pass;
+        Admin::$admins;
+
+    }
+
+    /**
+     * 
+     */
+    public function getId ()
+    {
+        return $this->id;
+    }
+
+    /**
+     * 
+     */
+    public function getName ()
+    {
+        return $this->name;
+    }
+
+    /**
+     * 
+     */
+    public function getPass ()
+    {
+        return $this->pass;
+    }
+
+
+}
